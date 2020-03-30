314ecfbf41572871248f914fb633099de9a66c70
ztm <mzdxztm@163.com>
2020-03-30 16:00:31 +0800

    上次提交了，但是有些类没能提交上去。

---
+++ library/src/main/java/com/mzdxztm/ztm/library/net/FreeApi.java
@@ -0,0 +1,12 @@
+package com.mzdxztm.ztm.library.net;
+
+public interface FreeApi {
+    /**
+     * 手机归属地：http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=手机号
+     *
+     * 物流接口：http://www.kuaidi100.com/query?type=快递公司代号&postid=快递单号
+     *           测试http://www.kuaidi100.com/query?type=yuantong&postid=11111111111
+     *
+     *  爱奇艺视频：http://cache.video.iqiyi.com/jp/avlist/202861101/1/?callback=jsonp9
+     */
+}
---
+++ library/src/test/java/com/mzdxztm/ztm/library/Paper.java
@@ -0,0 +1,8 @@
+package com.mzdxztm.ztm.library;
+
+/**
+ * 抄稿纸
+ */
+public class Paper {
+
+}
